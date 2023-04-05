5c5249998d6d59895bbf99548191b32dd0c079a8
pro100ton <pro100ton@gmail.com>
2023-04-05 22:29:13 +0300

    Initial commit

---
+++ ConsoleControl/ConsoleControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsoleControlApp.swift
+//  ConsoleControl
+//
+//  Created by Антон Шалимов on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConsoleControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
