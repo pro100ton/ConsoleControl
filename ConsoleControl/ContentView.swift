8244a2765fc440d97963800236ee13dc8647d602
pro100ton <pro100ton@gmail.com>
2023-04-05 23:20:01 +0300

    Add Login form to an App

--- ConsoleControl/ContentView.swift
+++ ConsoleControl/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    func signIn(){
+        print("Signed in with: \(username) and \(password)")
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.white, .blue],
+                           startPoint: .top,
+                           endPoint: .center)
+            .ignoresSafeArea()
+            VStack {
+                Text("Console control")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                TextField("Username", text: $username)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 1)
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 1)
+                Button("Login") {
+                    signIn()
+                }
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 50)
+                .background(.white)
+                .cornerRadius(10)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
